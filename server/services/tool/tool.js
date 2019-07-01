fdf5752f87a1cf7e0acdd8cc884affe6d09521e9
Zafar Saleem <zafarsaleem3@gmail.com>
2019-07-01 11:10:47 -0400

    Updated saved tool and user tool based on certain conditions

--- server/services/tool/tool.js
+++ server/services/tool/tool.js
@@ -1,31 +1,77 @@
 'use strict';
 
 const Tools = require('../../models/Tools');
+const UserTools = require('../../models/UserTools');
 
 function add(request, response) {
   const logoPath = `http://localhost:3001/src/components/admin/tool/assets/${request.body.name.charAt(0).toUpperCase()}${request.body.name.slice(1)}.png`;
-  const newTool = {
-    name: request.body.name.charAt(0).toUpperCase() + request.body.name.slice(1),
-    homePage: request.body.link,
-    logoPath: logoPath,
-    className: request.body.name.toLowerCase()
-  };
-
-  new Tools(newTool).save().then(doc => {
-    let newToolToJSON = doc.toJSON();
-    newToolToJSON['success'] = true;
-    newToolToJSON['message'] = 'New Tool Successfully Added';
-
-    return response.json(newToolToJSON);
-  }).catch(error => {
-    return response.json({
-      success: false,
-      messages: [
-        error.errors.name.message,
-        error.errors.homePage.message,
-        error.errors.className.message
-      ]
-    });
+  Tools.findOne({ name: request.body.name }, (error, tool) => {
+    if (error) return response.json(error);
+    if (!tool) {
+      new Tools({
+        name: request.body.name.charAt(0).toUpperCase() + request.body.name.slice(1),
+        homePage: request.body.link,
+        logoPath: logoPath,
+        className: request.body.name.toLowerCase()
+      })
+      .save()
+      .then(doc => {
+        new UserTools({
+          toolName: request.body.name.charAt(0).toUpperCase() + request.body.name.slice(1),
+          toolID: doc._id,
+          userID: request.body.userID
+        })
+        .save()
+        .then(doc => {
+          let newToolToJSON = doc.toJSON();
+          newToolToJSON['success'] = true;
+          newToolToJSON['message'] = 'New Tool Successfully Added';
+          return response.json(newToolToJSON);
+        })
+        .catch(error => {
+          return response.json({
+            success: false,
+            messages: [
+              error.errors.toolName.message,
+              error.errors.toolID.message,
+              error.errors.userID.message
+            ]
+          });
+        })
+      }).catch(error => {
+        return response.json({
+          success: false,
+          messages: [
+            error.errors.name.message,
+            error.errors.homePage.message,
+            error.errors.className.message
+          ]
+        });
+      })
+    } else {
+      new UserTools({
+        toolName: request.body.name.charAt(0).toUpperCase() + request.body.name.slice(1),
+        toolID: tool._id,
+        userID: request.body.userID
+      })
+      .save()
+      .then(doc => {
+        let newToolToJSON = doc.toJSON();
+        newToolToJSON['success'] = true;
+        newToolToJSON['message'] = 'New Tool Successfully Added';
+        return response.json(newToolToJSON);
+      })
+      .catch(error => {
+        return response.json({
+          success: false,
+          messages: [
+            error.errors.toolName.message,
+            error.errors.toolID.message,
+            error.errors.userID.message
+          ]
+        });
+      })
+    }
   });
 }
 
