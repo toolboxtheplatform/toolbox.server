9ac015db551bab42051443f5c67116dfc766244f
Zafar Saleem <zafarsaleem3@gmail.com>
2019-07-01 11:10:04 -0400

    Marked name as unique

--- server/models/Tools.js
+++ server/models/Tools.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const ToolsSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   homePage: {
     type: String,
