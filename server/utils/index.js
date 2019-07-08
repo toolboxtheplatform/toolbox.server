d33a10b9523e2f8d9dac05d28771af2f110b4f85
Zafar Saleem <zafarsaleem3@gmail.com>
2019-07-08 12:36:41 -0400

    Utility function to check if the request is sent by admin or employee

---
+++ server/utils/index.js
@@ -0,0 +1,18 @@
+const User = require('../models/User');
+
+function isAdmin(id) {
+  return new Promise((resolve, reject) => {
+    User.findOne({ _id: id }, (error, doc) => {
+      if (error) reject(error);
+      if (doc.role === 'Admin' || doc.role === 'admin') resolve(true);
+      reject({ 
+        success: false,
+        message: 'This is a restricted area and can only be access by Admins.'
+      });
+    });
+  });
+}
+
+module.exports = {
+  isAdmin: isAdmin
+};
